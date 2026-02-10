08368d973e9861c836b17ba91f519250ecac0e30
terrence.gonsalves <terrence.gonsalves@gmail.com>
2026-02-10 12:47:20 -0500

    redirect to dashboard not upload

--- src/pages/auth/callback.tsx
+++ src/pages/auth/callback.tsx
@@ -224,7 +224,7 @@ export default function AuthCallback() {
                 }
 
                 // redirect to upload page
-                router.push("/upload");
+                router.push("/dashboard");
             } else {
                 router.push("/login");
             }
